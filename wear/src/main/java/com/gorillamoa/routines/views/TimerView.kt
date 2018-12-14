bcb42f683c32df02a510d359bcdd9d9dc60e07c2
Guillermo Alvarez Colman <alvarezgd@Guillermos-MBP.hitronhub.home>
2018-12-13 18:26:23 -0800

    added red ring

--- wear/src/main/java/com/gorillamoa/routines/views/TimerView.kt
+++ wear/src/main/java/com/gorillamoa/routines/views/TimerView.kt
@@ -2,14 +2,58 @@ package com.gorillamoa.routines.views
 
 import android.content.Context
 import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
-class TimerView @JvmOverloads constructor(
-        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
-) : View(context, attrs, defStyleAttr) {
+class TimerView : View {
 
-    override fun onDraw(canvas: Canvas?) {
+    var circleColor = DEFAULT_CIRCLE_COLOR
+        set(circleColor) {
+            field = circleColor
+            invalidate()
+        }
+    private var paint: Paint? = null
+
+    constructor(context: Context) : super(context) {
+        init(context, null)
+    }
+
+    constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+        init(context, attrs)
+    }
+
+    private fun init(context: Context, attrs: AttributeSet?) {
+        paint = Paint()
+        paint!!.isAntiAlias = true
+        paint!!.style = Paint.Style.STROKE
+        paint!!.strokeWidth = 3f
+    }
+
+    override fun onDraw(canvas: Canvas) {
         super.onDraw(canvas)
+
+        val w = width
+        val h = height
+
+        val pl = paddingLeft
+        val pr = paddingRight
+        val pt = paddingTop
+        val pb = paddingBottom
+
+        val usableWidth = w - (pl + pr)
+        val usableHeight = h - (pt + pb)
+
+        val radius = (Math.min(usableWidth, usableHeight) / 2)-5
+        val cx = pl + usableWidth / 2
+        val cy = pt + usableHeight / 2
+
+        paint!!.color = this.circleColor
+        canvas.drawCircle(cx.toFloat(), cy.toFloat(), radius.toFloat(), paint!!)
+    }
+
+    companion object {
+        private val DEFAULT_CIRCLE_COLOR = Color.RED
     }
 }
