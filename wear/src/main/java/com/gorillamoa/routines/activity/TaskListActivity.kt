a542aeeefc47a0c7b47e75fff3f23a95c8b4421e
Guillermo Alvarez Colman <alvarezgd@Guillermos-MacBook-Pro.local>
2019-03-29 17:20:43 -0400

    made new task list activity

--- wear/src/main/java/com/gorillamoa/routines/activity/TaskListActivity.kt
+++ wear/src/main/java/com/gorillamoa/routines/activity/TaskListActivity.kt
@@ -1,14 +1,13 @@
-package com.gorillamoa.routines
+package com.gorillamoa.routines.activity
 
 import android.os.Bundle
 import android.support.wearable.activity.WearableActivity
 
-class WearRunnerActivity : WearableActivity() {
+class TaskListActivity : WearableActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_wear_runner)
-
+        setContentView(R.layout.activity_task_list)
 
         // Enables Always-on
         setAmbientEnabled()
