b9f304336c54df69b62d083469df2d5582a1828a
Guillermo Alvarez Colman <alvarezgd@Guillermos-MacBook-Pro.local>
2019-03-30 17:59:15 -0400

    added header to list

--- wear/src/main/java/com/gorillamoa/routines/adapter/TaskListAdapter.kt
+++ wear/src/main/java/com/gorillamoa/routines/adapter/TaskListAdapter.kt
@@ -1,5 +1,6 @@
 package com.gorillamoa.routines.adapter
 
+import android.graphics.Typeface
 import android.text.Html
 import android.view.LayoutInflater
 import android.view.View
@@ -9,7 +10,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.gorillamoa.routines.R
 import com.gorillamoa.routines.data.Task
 
-class TaskListAdapter(private val callback:(Int)->Unit): RecyclerView.Adapter<TaskListAdapter.TaskItemHolder>() {
+class TaskListAdapter(private val callback:(Int)->Unit): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
 
      var tasks:List<Task>? = null
         set(value) {
@@ -20,30 +21,59 @@ class TaskListAdapter(private val callback:(Int)->Unit): RecyclerView.Adapter<Ta
 
     val builder = StringBuilder()
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskItemHolder {
-        return TaskItemHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false))
+    companion object {
+        const val VIEW_TYPE_TASK = 0
+        const val VIEW_TYPE_TITLE = 1
     }
 
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return  if(viewType == VIEW_TYPE_TASK)
+            TaskItemHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_task, parent, false))
+        else {
+            TitleViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.item_list_header, parent, false))
+        }
+    }
+
+    /**
+     * get the number of items to display.
+     * note that we show an extra for the title, hence the +1
+     */
     override fun getItemCount(): Int {
-        return tasks?.size ?:0
+        return (tasks?.size ?:0) + 1
     }
 
-    override fun onBindViewHolder(holder: TaskItemHolder, position: Int) {
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
         tasks?.let {
 
-            val task =  tasks!![position]
-            builder.clear()
-            builder.append("&#9999;&nbsp;")
-            builder.append(task.name)
+            if (holder is TitleViewHolder) {
+                holder.headerTextView.text = holder.headerTextView.context.getString(R.string.task_list_title)
+                holder.headerTextView.setTypeface(holder.headerTextView.typeface, Typeface.BOLD)
+            }
 
-            holder.tasKTextView.text = Html.fromHtml(builder.toString(),Html.FROM_HTML_MODE_COMPACT)
-            holder.tasKTextView.setOnClickListener {
-                callback.invoke(task.id?:-1)
+            if (holder is TaskItemHolder) {
+                val task =  tasks!![position - 1]
+                builder.clear()
+                builder.append("&#9999;&nbsp;")
+                builder.append(task.name)
+
+                holder.tasKTextView.text = Html.fromHtml(builder.toString(),Html.FROM_HTML_MODE_COMPACT)
+                holder.tasKTextView.setOnClickListener {
+                    callback.invoke(task.id?:-1)
+                }
             }
         }
     }
 
+    override fun getItemViewType(position: Int): Int {
+        return if(position == 0 ) VIEW_TYPE_TITLE else VIEW_TYPE_TASK
+    }
+
+
     inner class TaskItemHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
          var tasKTextView: TextView = itemView.findViewById(R.id.taskNameTextView)
+    }
+
+    inner class TitleViewHolder(item:View):RecyclerView.ViewHolder(item){
+        var headerTextView = item.findViewById<TextView>(R.id.headerTextView)
     }
 }
