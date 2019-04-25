2eae415ebefe574b807a1b9f93a32e9930382cb0
Guillermo Alvarez Colman <alvarezgd@Guillermos-MacBook-Pro.local>
2019-04-25 19:15:00 -0400

    added delauney background

--- wear/src/main/java/com/gorillamoa/routines/TaskWatchService.kt
+++ wear/src/main/java/com/gorillamoa/routines/TaskWatchService.kt
@@ -28,10 +28,16 @@ import com.gorillamoa.routines.views.CanvasButton
 import com.gorillamoa.routines.views.ClickableRectangle
 import com.gorillamoa.routines.views.SwitchingButton
 import com.gorillamoa.routines.views.TimerView
+import io.github.jdiemke.triangulation.Vector2D
 
 import java.lang.ref.WeakReference
 import java.util.*
 import kotlin.math.roundToInt
+import io.github.jdiemke.triangulation.NotEnoughPointsException
+import io.github.jdiemke.triangulation.Triangle2D
+import io.github.jdiemke.triangulation.DelaunayTriangulator
+
+
 
 
 
@@ -475,7 +481,7 @@ class TaskWatchService : CanvasWatchFaceService() {
             val intermidiateBitmap = Bitmap.createBitmap(width.toInt(),height.toInt(),Bitmap.Config.ARGB_8888)
             val canvas = Canvas(intermidiateBitmap)
             val lab = ColorSpace.get(ColorSpace.Named.CIE_LAB)
-            val alpha = 40.0f
+            val alpha = 255.0f
 //            val alpha = 10.0f
 
 
@@ -483,78 +489,118 @@ class TaskWatchService : CanvasWatchFaceService() {
             canvas.drawColor(Color.WHITE)
 
             val painter =Paint().apply {
-                style = Paint.Style.FILL
+                style = Paint.Style.FILL_AND_STROKE
                 isAntiAlias = true
+                strokeWidth = 2.0f
             }
 
             //TODO look up how to spread colors better
             //we'll make a gradient of 4 colors for now,
             val topLeft = Color.valueOf(255.0f,245.0f,235.0f,alpha,lab)
-            val bottomRight = Color.valueOf(200.0f,190.0f,180.0f, alpha,lab)
+            val bottomRight = Color.valueOf(127.0f,39.0f,4.0f, alpha,lab)
             val bottomLeft = Color.valueOf(175.0f,111.0f,84.0f, alpha,lab)
             val topRight = Color.valueOf(175.0f,111.0f,84.0f,alpha,lab)
 
             //we'll create delayney triangles
             val POINTS =61
-            val MAXTRIANGLES = 110
 
             val random = Random()
 
             //initialize an empty array of floating points to mark the vertices of our triangles
-            val point2ds = Array(POINTS){PointF(0.0f,0.0f)}
+            val point2ds = Vector<Vector2D>(POINTS)
 
             //place points on the corners of our quad
-            point2ds[0].x = 0.0f
-            point2ds[0].y = 0.0f
-            point2ds[1].x = 1.0f
-            point2ds[1].y = -1.0f
-            point2ds[2].x = -1.0f
-            point2ds[2].y = 1.0f
-            point2ds[3].x = 1.0f
-            point2ds[3].y = 1.0f
+            point2ds.addElement(Vector2D(0.0,0.0))
+            point2ds.addElement(Vector2D(width.toDouble(),height.toDouble()))
+            point2ds.addElement(Vector2D(0.0,height.toDouble()))
+            point2ds.addElement(Vector2D(width.toDouble(),0.0))
+
+            val halfWidth = width.toDouble().times(0.5)
+            val halfHeight = height.toDouble().times(0.5)
 
             for (i in 4 until POINTS) {
 
                 //place points on the edges of the quad
                 if (i < 8) {
-                    point2ds[i].x = 2.0f * random.nextFloat()  - 1.0f
-                    point2ds[i].y = -1.0f
+                    //left edge
+                    point2ds.addElement(Vector2D(  0.0, random.nextDouble() * height))
                 } else if (i < 12) {
-                    point2ds[i].x = (2.0f * random.nextFloat()) - 1.0f
-                    point2ds[i].y = 1.0f
+                    //top edge
+                    point2ds.addElement(Vector2D((width.toDouble() * random.nextDouble()), height.toDouble()))
                 } else if (i < 16) {
-                    point2ds[i].x = 1.0f
-                    point2ds[i].y = 2.0f * random.nextFloat() - 1.0f
+                    //right edge
+                    point2ds.addElement(Vector2D(width.toDouble(), random.nextDouble() * height.toDouble()))
                 } else if (i < 20) {
-                    point2ds[i].x = -1.0f
-                    point2ds[i].y = (2.0f * random.nextFloat())  - 1.0f
-
+                    //bottom edge
+                    point2ds.addElement(Vector2D(width.toDouble() * random.nextDouble(), 0.0))
 
                     //in order to disperse points more evenly across the entire quad, the quad is split into 4 quadrants
                     // and random points are generated within each quadrant
                 } else if (i < 30) {
-                    point2ds[i].x = random.nextFloat()
-                    point2ds[i].y = random.nextFloat()
+                    //bottom left quadrant
+                    point2ds.addElement(Vector2D(random.nextDouble() * halfWidth, random.nextDouble() * halfHeight))
                 } else if (i < 40) {
-                    point2ds[i].x = random.nextFloat() - 1.0f
-                    point2ds[i].y = random.nextFloat()
+                    //top left quadrant
+                    point2ds.addElement(Vector2D(random.nextDouble() *halfWidth, (random.nextDouble() * halfHeight) + halfHeight))
                 } else if (i < 50) {
-                    point2ds[i].x = random.nextFloat()  - 1.0f
-                    point2ds[i].y = random.nextFloat()  - 1.0f
+                    //top right quadrant
+                    point2ds.addElement(Vector2D((random.nextDouble() * halfWidth) + halfWidth, (random.nextDouble()*halfHeight + halfHeight) ))
                 } else if (i < 60) {
-                    point2ds[i].x = random.nextFloat()
-                    point2ds[i].y = random.nextFloat()  - 1.0f
+                    //bottom right quadrant
+                    point2ds.addElement(Vector2D((random.nextDouble() * halfWidth) + halfWidth , random.nextDouble() * halfHeight))
                 }
-                
             }
 
+            val triangleSoup:List<Triangle2D>? = try {
+                val delaunayTriangulator = DelaunayTriangulator(point2ds)
+                delaunayTriangulator.triangulate()
+                delaunayTriangulator.triangles as List<Triangle2D>
+
+            } catch (e: NotEnoughPointsException) {
+                Log.d("$tag generateBackgroundImage","Woops Triangulation")
+                null
+
+            }
+
+            var centerX:Double
+            var centerY:Double
+            val path=Path()
+            path.fillType = Path.FillType.EVEN_ODD
+
+            triangleSoup?.let {
+
+                //TODO work with floats because we don't need double precision
+                triangleSoup.forEach {
+
+//                    Log.d("$tag generateBackgroundImage","Triangle: A(${it.a.x},${it.a.y}) B(${it.b.x},${it.b.y}) C(${it.c.x},${it.c.y})")
+
+                    //first find the Center Coordinates
+                    centerX = (it.a.x + it.b.x + it.c.x).div(3.0)
+                    centerY = (it.a.y + it.b.y + it.c.y).div(3.0)
+
+//                    Log.d("$tag generateBackgroundImage","Centroid: $centerX, $centerY")
+
+                    //now use the coordinates to locate the correct color
+                    val colorLeft = topLeft.lerp(bottomLeft,centerY.toFloat()/height,lab)
+                    val colorRight = topRight.lerp(bottomRight,centerY.toFloat()/height,lab)
+                    val final =  colorLeft.lerp(colorRight,centerX.toFloat()/width,lab)
+                    painter.color = Color.argb(final.alpha().roundToInt(),final.red().roundToInt(), final.green().roundToInt(), final.blue().roundToInt())
 
 
+                    //draw the centroids
+                  //  canvas.drawPoint(centerX.toFloat(),centerY.toFloat(),painter)
 
+                    //now we draw the triangle
+                    path.moveTo(it.a.x.toFloat(),it.a.y.toFloat())
+                    path.lineTo(it.b.x.toFloat(),it.b.y.toFloat())
+                    path.lineTo(it.c.x.toFloat(),it.c.y.toFloat())
+                    path.lineTo(it.a.x.toFloat(),it.a.y.toFloat())
 
-
-
-
+                    canvas.drawPath(path,painter)
+                    path.reset()
+                }
+            }
+            //Determine the center position of each triangle
 
 //                canvas.drawCircle(x,y,radius,painter)
 
