ba288bdcf33c98586d506231ccc1125b55cea5a8
Guillermo Alvarez Colman <alvarezgd@Guillermos-MacBook-Pro.local>
2019-06-19 12:35:13 -0400

    added some events to the data layer listener

--- wear/src/main/java/com/gorillamoa/routines/services/DataLayerListenerService.kt
+++ wear/src/main/java/com/gorillamoa/routines/services/DataLayerListenerService.kt
@@ -2,26 +2,118 @@ package com.gorillamoa.routines.services
 
 import android.util.Log
 import android.widget.Toast
+import com.google.android.gms.wearable.DataEvent
 import com.google.android.gms.wearable.DataEventBuffer
+import com.google.android.gms.wearable.MessageEvent
 import com.google.android.gms.wearable.WearableListenerService
 
 /**
  * Listens for data changes (in case we are synchronized with the mobile)
  */
+
+//TODO SYNCRHONIZE WAKE UP EVENT
+//TODO SYNCRHONIZE TASK ACTIONS FINISH/UNFINISH/SKIP
+//TODO SYNCRHONIZE HISTORY
+//TODO SYNCHRONIZE CURRENT VISIBLE TASK
+//TODO SYNCHRONIZE SLEEP
+
 class DataLayerListenerService:WearableListenerService(){
 
     @Suppress("unused")
     private val tag:String = DataLayerListenerService::class.java.name
 
+    companion object {
+
+        /**
+         * Another device has received the wake up event, so lets show the wake up
+         * event here as well
+         */
+        const val EVENT_WAKEUP = "event.wakeup"
+
+        /**
+         * Another device has received the sleep event, so lets show the sleep
+         * notification here as well
+         */
+        const val EVENT_SLEEP = "event.sleep"
+
+        /**
+         * A task has its history updated. Update the local database task
+         */
+        const val EVENT_TASK_HISTORY_CHANGED = "event.task_info_change"
+
+        /**A task has its info changed
+         * One of: frequency, name, type has changed so update the local database
+         */
+        const val EVENT_TASK_INFO_CHANGED ="event.task_settings_change"
+
+        /**
+         * The user's history info has updated
+         * One of: wake history, sleep history, task completion history
+         */
+        const val EVENT_USER_HISTORY_UPDATED = "event.user_history_UPDATED"
+
+        /**
+         * The user has reacted to the wake up event by Starting the day.
+         * This action will require syncing of completed/uncompleted task lists
+         */
+        const val ACTION_WAKE = "action.startday"
+
+        /**
+         * The user has reacted to the sleep event.
+         * mark what the user has done: One of Sleep, snooze the sleep
+         */
+        const val ACTION_SLEEP = "action.sleep"
+
+        /**
+         * The user has marked a task finished.
+         * Sync completed/uncompleted task lists
+         */
+        const val ACTION_DONE = "action.done"
+
+        /**
+         * The user has marked a task unfinished.
+         * Sync the completed/uncompleted task list
+         */
+        const val ACTION_UNFINISHED = "action.uncomplete"
+
+        /**
+         * The user is now viewing a different task.
+         * Sync the "current" viewing task
+         */
+        const val ACTION_VIEW_TASK = "action.viewchange"
+
+
+    }
+
+    override fun onMessageReceived(messageEvent: MessageEvent?) {
+        super.onMessageReceived(messageEvent)
+
+
+
+    }
+
+
     override fun onDataChanged(dataEvents: DataEventBuffer) {
        Log.d("$tag onDataChanged","$dataEvents")
 
         Toast.makeText(applicationContext,"Received",Toast.LENGTH_SHORT).show()
-//        dataEvents.map { it.dataItem.uri}
-//                .forEach {
-//
-//                }
 
+        dataEvents.forEach {
+
+            when (it.type) {
+                DataEvent.TYPE_CHANGED -> {
+
+
+
+                }
+                DataEvent.TYPE_DELETED ->{
+
+                }
+                else -> {
+
+                }
+            }
+        }
     }
 
 }
